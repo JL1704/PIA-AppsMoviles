28ec383e306bc834d3413a399ea243328d2c5cb7
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-05-06 23:45:45 -0600

    Creating-the-project_#1

---
+++ miPIA-2132939/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miPIA-2132939',
+  webDir: 'www'
+};
+
+export default config;
