a5b368d09af5041c6912c23a457fc3e6ddcbc157
Jose Luis CG <142639379+JL1704@users.noreply.github.com>
2024-05-08 01:34:55 -0600

    Design-improvement-on-the-home-page_#6

--- miPIA-2132939/src/app/game-data.service.ts
+++ miPIA-2132939/src/app/game-data.service.ts
@@ -12,13 +12,13 @@ export class GameDataService {
   // Simular una lista de juegos en venta
   private gamesOnSale: Game[] = [
     {
-      title: 'Juego en Venta 1',
+      title: 'Juego 1',
       imageUrl: 'https://th.bing.com/th/id/R.197e9fb01725220408b2cfd56c66cea4?rik=NjsyFC%2fCSLHBtA&pid=ImgRaw&r=0',
       category: 'Categoría del Juego 1',
       price: 19.99
     },
     {
-      title: 'Juego en Venta 2',
+      title: 'Juego 2',
       imageUrl: 'https://th.bing.com/th/id/R.197e9fb01725220408b2cfd56c66cea4?rik=NjsyFC%2fCSLHBtA&pid=ImgRaw&r=0',
       category: 'Categoría del Juego 2',
       price: 24.99
